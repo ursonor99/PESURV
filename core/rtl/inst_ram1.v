03569489b6deef4a2b8e95aea04bcfee11f31643
ursonor99 <atrathish@gmail.com>
2021-06-15 17:14:15 +0530

    instr mem added exter

--- core/rtl/inst_ram1.v
+++ core/rtl/inst_ram1.v
@@ -36,16 +36,23 @@ module inst_ram1#(
     );
     input         clk;
     output [w-1:0] inst_data;
+
+    //from pc 
     input [w-1:0]  pc;
     input         re;
     input        is_write;
-    //input [w-1:0] im_addr;
+
+    //ext input
+    input [w-1:0] im_addr;
     input [w-1:0] im_inst;
     //output[31:0] im_dout;
     //output[31:0] im_dout2;
     
-    wire[w-1:0] shifted_addr;
-    assign shifted_addr[w-1:0]=pc>>2;
+    wire[w-1:0] shifted_addr_pc;
+    assign shifted_addr_pc[w-1:0]=pc>>2;
+
+    wire[w-1:0] shifted_addr_load;
+    assign shifted_addr_load[w-1:0]=pc>>2;
     
 integer i=0;
 (* ram_style = "block" *)reg [w-1:0] inst[2**h-1:0];
@@ -53,14 +60,14 @@ integer i=0;
 
 always@(posedge clk)begin
     if(is_write)begin
-        inst[shifted_addr][w-1:0]<=im_inst[w-1:0];
-        $display("%h %h %h %h",pc[w-1:0],shifted_addr[w-1:0],im_inst[w-1:0],inst[shifted_addr][w-1:0]);
+        inst[shifted_addr_load][w-1:0]<=im_inst[w-1:0];
+        $display("%h %h %h %h",shifted_addr_pc[w-1:0],shifted_addr_load[w-1:0],im_inst[w-1:0],inst[shifted_addr_load][w-1:0]);
         
     end
     
 end
 begin
-assign inst_data[w-1:0]=(re)?inst[shifted_addr][w-1:0]:2'h00;
+assign inst_data[w-1:0]=(re)?inst[shifted_addr_pc][w-1:0]:2'h00;
 
 end
 
