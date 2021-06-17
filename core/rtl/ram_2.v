552f0c55ce75351e70135481fba86a3ebf6beece
Giridhar11-hub <80198555+Giridhar11-hub@users.noreply.github.com>
2021-06-17 10:42:18 +0530

    Update ram_2.v

--- core/rtl/ram_2.v
+++ core/rtl/ram_2.v
@@ -18,12 +18,15 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
+`define BYTE 4'b0001
+`define HALFWORD 4'b0011
+`define THREEQUATER 4'b0111
+`define FULLWORD 4'b1111
 
 module ram_2 #(
     parameter w=32,
     parameter h=8,
-    parameter l=4
+    parameter l=3
     )(
     clk,
     ram_wdat,
@@ -42,11 +45,12 @@ module ram_2 #(
     input   wire  [l-1:0]   ram_type;
     input   wire   [w-1:0]  ram_addr;
     input   wire            ram_re;
-    output  reg     [w-1:0] data_reg;
+    output  wire     [w-1:0] data_reg;
     input wire sign;
     
 
-wire [w-1:0] data;
+
+
 
 //wire unsign=1'b1;
 
@@ -74,59 +78,82 @@ always@(posedge clk)
 begin
         if(ram_we)begin
             if(ram_type[0])
-                Bram[address1][h-1:0] <=ram_wdat[h-1:0];
+                Bram[address1]<=ram_wdat[h-1:0];
              
                 
             if(ram_type[1])
-                Bram[address2][h-1:0]<= ram_wdat[2*h-1:h];
+                Bram[address2]<= ram_wdat[2*h-1:h];
                 
            
 
             if(ram_type[2])
-                Bram[address3][h-1:0] <=ram_wdat[3*h-1:2*h];
+                Bram[address3]<=ram_wdat[3*h-1:2*h];
                 
             
 
             if(ram_type[3])
-                Bram[address4][h-1:0] <=ram_wdat[4*h-1:3*h];
+                Bram[address4]<=ram_wdat[4*h-1:3*h];
                 
-            //$display(Bram[address1]);
+            $display(Bram[address1]);
            
          end
 
 end
 
 
-always@*
-    begin:outputk
-    
-//    reg  [w-1:0] douta_reg={w{1'b0}};
-  //always@(posedge clk)
-    if(sign)
-        begin
-        case(ram_type)  
-                  4'b0001:data_reg[31:0]={{24{Bram[address1][7]}},Bram[address1]};
-                  4'b0010:data_reg[31:0]={{16{Bram[address2][7]}},Bram[address2],Bram[address1]};                                                                         
-                  4'b0100:data_reg[31:0]={{8{Bram[address3][7]}},Bram[address3],Bram[address2],Bram[address1]};                                            
-                  4'b1000:data_reg[31:0]={Bram[address4],Bram[address3],Bram[address2],Bram[address1]};
-         endcase 
-         end        
-   else
-        begin
-        case(ram_type)
-                4'b0001:data_reg[31:0]={24'b0,Bram[address1]};
-                4'b0010:data_reg[31:0]={16'b0,Bram[address2],Bram[address1]};     
-                4'b0100:data_reg[31:0]={8'b0,Bram[address3],Bram[address2],Bram[address1]};
-                4'b1000:data_reg[31:0]={Bram[address4],Bram[address3],Bram[address2],Bram[address1]};                                                             
-    
-        endcase
-   end
-   
-   
- end
+//read
+wire [h-1:0] data1;
+assign data1=Bram[address1];
+wire [h-1:0] data2;
+assign data2=Bram[address2];
+wire [h-1:0] data3;
+assign data3=Bram[address3];
+wire [h-1:0] data4;
+assign data4=Bram[address4];
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//byte
+wire [w-1:0]signedbyte;
+wire [w-1:0]unsignedbyte;
+
+//halfword
+wire [w-1:0]signedhalfword;
+wire [w-1:0]unsignedhalfword;
+
+//threequaters
+wire [w-1:0]signedthreequaters;
+wire [w-1:0]unsignedthreequaters;
+
+//fullword
+wire [w-1:0]fullword;
+
+//////////////////////////////////////////////////////////////////////////////////
+//sign extension
+assign signedbyte={{24{data1[7]}},data1};
+assign unsignedbyte={24'b0,data1};
+assign signedhalfword={16'b0,data2,data1};
+assign unsignedbyte={16'b0,data2,data1};
+assign signedthreequaters={{8{data3[7]}},data3,data2,data1};
+assign unsignedthreequaters={8'b0,data3,data2,data1};
+assign unsignedbyte={data4,data3,data2,data1};
+
+
+assign data_reg=(sign==1 && ram_type==`BYTE)?signedbyte:
+                (sign==0 && ram_type==`BYTE)?unsignedbyte:
+                (sign==1 && ram_type==`HALFWORD)?signedhalfword:
+                (sign==0 && ram_type==`HALFWORD)?unsignedhalfword:
+                (sign==1 && ram_type==`THREEQUATER)?unsignedthreequaters:
+                (sign==0 && ram_type==`THREEQUATER)?signedthreequaters:
+                (sign==0 || sign==1 &&ram_type==`FULLWORD)?fullword:
+                32'b0;
+
+
+
+
    
                                                  
 
            
 
 endmodule
+
